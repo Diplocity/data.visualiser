801f5fb064b85769ce65aae444c6ab0666b7db29
Ebenezer Edem Zuh <ebenezuh@gmail.com>
2024-03-09 17:14:59 +0100

    data visualizer

---
+++ Proj 3/medical_data_visualizer.py
@@ -0,0 +1,16 @@
+Initial commit
+
+# Please enter the commit message for your changes. Lines starting
+# with '#' will be ignored, and an empty message aborts the commit.
+#
+# interactive rebase in progress; onto 7150e06
+# Last command done (1 command done):
+#    pick e83773a Initial commit
+# No commands remaining.
+# You are currently rebasing branch 'main' on '7150e06'.
+#
+# Changes to be committed:
+#	new file:   .vscode/launch.json
+#	new file:   data.visualiser
+#	new file:   medical_data_visualizer.py
+#
